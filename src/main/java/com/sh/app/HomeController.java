b57274adf0554c1e05cae64d4436e1d0427dffa0
owj7182 <owj7182@naver.com>
2024-02-05 17:28:01 +0900

    Feat: ㅋㅋㅋ추가

--- src/main/java/com/sh/app/HomeController.java
+++ src/main/java/com/sh/app/HomeController.java
@@ -15,6 +15,6 @@ public class HomeController {
 
     @GetMapping("/")
     public ResponseEntity<?> home() {
-        return ResponseEntity.ok("ㅋㅋㅋㅋ현재 profile은 %s입니다.".formatted(value));
+        return ResponseEntity.ok("현재 profile은 %s입니다.ㅋㅋㅋ".formatted(value));
     }
 }
